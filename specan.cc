10890ab913c5489e0168b126ba558707cc868e98
Daniel Coderre <coderdj@gmail.com>
2014-06-19 11:48:33 +0200

    Better peak finding for low energy

--- specan.cc
+++ specan.cc
@@ -12,7 +12,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
    //process command line options
-   string sPath = "/home/dan/data/test";
+   string sPath = "/home/dan/data/TEST";
    double dThreshold=0.;
    int module=-1,channel=-1;
    bool bPlotOnline=false;
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
    TApplication *theApp = new TApplication("App",&argc,argv);
    TCanvas *can = new TCanvas("can","can");
    //
-   TH1F *spectrum = new TH1F("spectrum","",10000,0,1000000);
+   TH1F *spectrum = new TH1F("spectrum","",100100,-100,100100);
    //
    
    can->cd();
@@ -86,36 +86,61 @@ int main(int argc, char *argv[])
 	    u_int32_t *easierData = (u_int32_t*)data;
 	    int hChannelID=0,hModuleID=0;
 	    FileReader.channel_id(x,hChannelID,hModuleID);
-	    
+
 	    if((channel!=-1 && module==-1 && channel!=hChannelID) ||
 	       (channel!=-1 && module!=-1 && channel!=hChannelID && module!=hModuleID))
 	      continue;
 	    
 	    int    bin=0;	    
+	    int    lastBin=-1;
 	    double baseline=0.;
 	    double total=0.;
 	    double lastTwo=0.;
-	    
-	    for(unsigned int z=0;z<datasize/2;z+=2)  {
-	       lastTwo=0.;
-	       if(bin<4){
-		  baseline+=easierData[z/2]&0xFFFF;
-		  baseline+=(easierData[z/2]>>16)&0xFFFF;
-	       }
-	       //if(bin==4)
-	       //baseline/=4.;
-	       total+=easierData[z/2]&0xFFFF;
-	       total+=(easierData[z/2]>>16)&0xFFFF;
-	       lastTwo+=easierData[z/2]&0xFFFF;
-	       lastTwo+=(easierData[z/2]>>16)&0xFFFF;
-	       bin+=2;
-	    }	 	 
-	    baseline/=4.;
-	    total-=((double)bin)*baseline;
+	    int lowestBin=-1;
+	    int lowestValue=100000;
+	    for(unsigned int z=0;z<datasize/2;z+=2)  {	     
+	      lastTwo=0.;
+	      if(bin<10){
+		baseline+=easierData[z/2]&0xFFFF;
 
+		baseline+=(easierData[z/2]>>16)&0xFFFF;
+	      }
+	      //if(bin==4)
+	      //baseline/=4.;
+	      total+=easierData[z/2]&0xFFFF;
+	      total+=(easierData[z/2]>>16)&0xFFFF;
+	      lastTwo+=easierData[z/2]&0xFFFF;
+	      lastTwo+=(easierData[z/2]>>16)&0xFFFF;
+	      bin+=2;
+	      lastBin=bin;
+
+	      //other method
+	      if((easierData[z/2]&0xFFFF)<lowestValue) { 
+		lowestValue=(easierData[z/2]&0xFFFF);
+		lowestBin=z;
+	      }
+	      if(((easierData[z/2]>>16)&0xFFFF)<lowestValue) {
+		lowestValue=((easierData[z/2]>>16)&0xFFFF);
+		lowestBin=z;
+	      }
+	    }	 	 
+	    baseline/=10.;
+	    //baseline=16386;
+	    total-=((double)bin)*baseline;
+	    //	    cout<<baseline<<endl;
 	    //condition on last two bins being near baseline
-	    if(total<0 && TMath::Abs((lastTwo/2.)-baseline<100))
-	      spectrum->Fill((total*-1.));	 
+	    //if(total<0 && TMath::Abs((lastTwo/2.)-baseline<100))
+	    if(lowestBin>5 && lowestBin<lastBin-5){
+	      double tot=0.;
+	      for(unsigned int z=lowestBin-3;z<lowestBin+3;z++){
+		tot+=easierData[z/2]&0xFFFF;
+		tot+=(easierData[z/2]>>16)&0xFFFF;
+	      }	    
+	      if((tot-baseline*12)*-1.>30000)
+		cout<<hEventNumber<<" NUMBER"<<" TOT: "<<tot<<" BL: "<<baseline<<endl;
+	      spectrum->Fill((tot-baseline*12)*-1.);
+	    }
+	    //spectrum->Fill((total*-1.));	 
 	 }	 
 	 if(bPlotOnline && hEventNumber%10000==0){	      
 	    spectrum->Draw();
